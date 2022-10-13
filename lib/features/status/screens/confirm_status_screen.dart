b5e932b53f47d2c6ec9da6e030e221397dcf843a
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-10-14 02:44:16 +0600

    Who can see empty list bug fixed

--- lib/features/status/screens/confirm_status_screen.dart
+++ lib/features/status/screens/confirm_status_screen.dart
@@ -9,6 +9,7 @@ import 'package:wechat/common/widgets/error.dart';
 import 'package:wechat/common/widgets/loader.dart';
 import 'package:wechat/features/select_contact/controller/select_contacts_controller.dart';
 import 'package:wechat/features/status/controller/status_controller.dart';
+import 'package:wechat/models/user_model.dart';
 
 class ConfirmStatusScreen extends ConsumerStatefulWidget {
   static const String routeName = '/confirm-status-screen';
@@ -24,6 +25,7 @@ class _ConfirmStatusScreenState extends ConsumerState<ConfirmStatusScreen> {
   final TextEditingController _messageController = TextEditingController();
   bool wideImage = false;
   bool isShowEmojiContainer = false;
+  List<String> whoCanSee = [];
   FocusNode focusNode = FocusNode();
 
   void isWidder() async {
@@ -60,22 +62,22 @@ class _ConfirmStatusScreenState extends ConsumerState<ConfirmStatusScreen> {
     }
   }
 
-  void uploadStatus() {
-    ref.read(getSelectContactProvider).when(
-        data: (contactList) {
-          List<String> whoCanSee = [];
-          for (var list in contactList) {
-            whoCanSee.add(list.uid);
-          }
-          ref.read(statusControllerProvider).uploadStatus(
-              file: widget.file,
-              type: MessageEnum.image,
-              caption: _messageController.text.trim(),
-              whoCanSee: whoCanSee,
-              context: context);
-        },
-        error: (err, trace) => ErrorScreen(error: err.toString()),
-        loading: (() => const Loader()));
+  void uploadStatus(List<UserModel> contactList) {
+    for (var list in contactList) {
+      whoCanSee.add(list.uid);
+    }
+    ref.read(statusControllerProvider).uploadStatus(
+        file: widget.file,
+        type: MessageEnum.image,
+        caption: _messageController.text.trim(),
+        whoCanSee: whoCanSee,
+        context: context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isWidder();
   }
 
   @override
@@ -86,109 +88,112 @@ class _ConfirmStatusScreenState extends ConsumerState<ConfirmStatusScreen> {
 
   @override
   Widget build(BuildContext context) {
-    isWidder();
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Stack(
-        children: [
-          const Center(
-            child: Loader(),
-          ),
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: wideImage ? BoxFit.fitWidth : BoxFit.cover,
-                    image: FileImage(widget.file))),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: isShowEmojiContainer ? 0 : 30),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        focusNode: focusNode,
-                        controller: _messageController,
-                        style: const TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          prefixIcon: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 5.0),
-                            child: SizedBox(
-                              width: 50,
-                              child: IconButton(
-                                onPressed: () {
-                                  toggleEmojiKeyboardContainer();
-                                },
-                                icon: const Icon(
-                                  Icons.emoji_emotions_outlined,
-                                  color: Colors.grey,
+      body: ref.watch(getSelectContactProvider).when(
+          data: (contactList) => Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: wideImage ? BoxFit.fitWidth : BoxFit.cover,
+                            image: FileImage(widget.file))),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            bottom: isShowEmojiContainer ? 0 : 30),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                focusNode: focusNode,
+                                controller: _messageController,
+                                style: const TextStyle(color: Colors.black),
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  prefixIcon: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5.0),
+                                    child: SizedBox(
+                                      width: 50,
+                                      child: IconButton(
+                                        onPressed: () {
+                                          toggleEmojiKeyboardContainer();
+                                        },
+                                        icon: const Icon(
+                                          Icons.emoji_emotions_outlined,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  hintText: 'Add a caption',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    borderSide: const BorderSide(
+                                      width: 0,
+                                      style: BorderStyle.none,
+                                    ),
+                                  ),
+                                  contentPadding: const EdgeInsets.all(10),
                                 ),
                               ),
                             ),
-                          ),
-                          hintText: 'Add a caption',
-                          hintStyle: const TextStyle(color: Colors.grey),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(50.0),
-                            borderSide: const BorderSide(
-                              width: 0,
-                              style: BorderStyle.none,
+                            const SizedBox(width: 5),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 8,
+                                right: 2,
+                                left: 2,
+                              ),
+                              child: GestureDetector(
+                                onTap: (() {
+                                  uploadStatus(contactList);
+                                }),
+                                child: const CircleAvatar(
+                                  backgroundColor: tabColor,
+                                  radius: 25,
+                                  child: Icon(
+                                    Icons.send,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
-                          contentPadding: const EdgeInsets.all(10),
+                          ],
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 5),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 8,
-                        right: 2,
-                        left: 2,
-                      ),
-                      child: GestureDetector(
-                        onTap: (() => uploadStatus()),
-                        child: const CircleAvatar(
-                          backgroundColor: tabColor,
-                          radius: 25,
-                          child: Icon(
-                            Icons.send,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                      isShowEmojiContainer
+                          ? SizedBox(
+                              height: 310,
+                              child: EmojiPicker(
+                                onEmojiSelected: (category, emoji) {
+                                  setState(() {
+                                    _messageController.text =
+                                        _messageController.text + emoji.emoji;
+                                  });
+                                },
+                              ),
+                            )
+                          : const SizedBox()
+                    ],
+                  ),
+                ],
               ),
-              isShowEmojiContainer
-                  ? SizedBox(
-                      height: 310,
-                      child: EmojiPicker(
-                        onEmojiSelected: (category, emoji) {
-                          setState(() {
-                            _messageController.text =
-                                _messageController.text + emoji.emoji;
-                          });
-                        },
-                      ),
-                    )
-                  : const SizedBox()
-            ],
-          ),
-        ],
-      ),
+          error: (err, trace) => ErrorScreen(error: err.toString()),
+          loading: () => const Loader()),
     );
   }
 }
