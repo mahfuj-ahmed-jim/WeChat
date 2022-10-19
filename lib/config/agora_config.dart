dca95e9706738e6cf625cf1f76d0a0f815dfe2f3
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-10-19 15:02:45 +0600

    Agora Dashboard & Credential

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = 'f243c0900a684e99bdbaa44b504a31a3';
+  static String appCertificaate = '9762f9ced65d4ac2aab967351193ab5d';
+}
